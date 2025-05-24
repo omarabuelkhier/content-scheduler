eaf50a864c0baccf4ea66e50f9ca498cfcb1ba39
omarabuelkhier <omarabuelkhier@gmail.com>
2025-05-24 16:21:20 +0300

    Add BaseRepository and BaseRepositoryInterface for standardized data handling

---
+++ app/Repositories/BaseRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Repositories;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BaseRepository implements BaseRepositoryInterface
+{
+    protected Model $model;
+
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    public function all()
+    {
+        return $this->model->all();
+    }
+
+    public function find(int $id)
+    {
+        return $this->model->findOrFail($id);
+    }
+
+    public function create(array $data)
+    {
+        return $this->model->create($data);
+    }
+
+    public function update(int $id, array $data)
+    {
+        $model = $this->find($id);
+        $model->update($data);
+        return $model;
+    }
+
+    public function delete(int $id)
+    {
+        $model = $this->find($id);
+        return $model->delete();
+    }
+}
