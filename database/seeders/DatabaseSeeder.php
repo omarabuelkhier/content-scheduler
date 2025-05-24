7ff9cbbdb3a4c410271086348f8d00ca2a030ab6
omarabuelkhier <omarabuelkhier@gmail.com>
2025-05-24 13:33:18 +0300

    Refactor DatabaseSeeder to use seeders for initial data population

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->call([
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            // Seeders for initial data
+            UserSeeder::class,
+            PlatformSeeder::class,
+            PostSeeder::class
+            
         ]);
     }
 }
