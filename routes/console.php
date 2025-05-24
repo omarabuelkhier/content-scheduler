997df156350db0082db51f8b4ba0270e5ebd4efb
omarabuelkhier <omarabuelkhier@gmail.com>
2025-05-24 15:06:05 +0300

    Schedule PublishScheduledPosts job to run every minute

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Jobs\PublishScheduledPosts;
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Schedule the PublishScheduledPosts job
+app(Schedule::class)->job(new PublishScheduledPosts)->everyMinute();
+// // Example of a scheduled command : php artisan schedule:work
