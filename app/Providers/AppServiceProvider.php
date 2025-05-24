f8ca88cd541c959d742755c0d32d6c1d7625150d
omarabuelkhier <omarabuelkhier@gmail.com>
2025-05-24 15:05:10 +0300

    Register ResponseHelper as a singleton in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Helpers\ResponseHelper;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +12,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(ResponseHelper::class, function ($app) {
+            return new ResponseHelper();
+        });
     }
 
     /**
