58efa14b2bf92c15c1f4b6d0e63b3308e3d47cda
omarabuelkhier <omarabuelkhier@gmail.com>
2025-05-24 15:05:28 +0300

    Refactor PlatformController to utilize ResponseHelper for standardized responses and improve platform retrieval logic

--- app/Http/Controllers/API/PlatformController.php
+++ app/Http/Controllers/API/PlatformController.php
@@ -2,31 +2,46 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Helpers\ResponseHelper;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\PlatformRequests\TogglePlatformRequest;
+use App\Http\Resources\PlatformResource;
 use App\Models\Platform;
-use Illuminate\Http\Request;
 
 class PlatformController extends Controller
 {
-    public function index()
+    protected $responseHelper;
+
+    public function __construct(ResponseHelper $responseHelper)
     {
-        return response()->json(Platform::all());
+        $this->responseHelper = $responseHelper;
     }
 
-    public function toggle(Request $request)
+    public function index()
     {
-        $request->validate([
-            'platform_id' => 'required|exists:platforms,id',
-        ]);
+        $platforms = Platform::all();
 
+        if ($platforms->isEmpty()) {
+            return $this->responseHelper->error('No platforms found', 200);
+        }
+
+        return $this->responseHelper->success(
+            'Platforms retrieved successfully',
+            PlatformResource::collection($platforms)
+        );
+    }
+
+    public function toggle(TogglePlatformRequest $request)
+    {
         $user = $request->user();
-
+        $request->validated();
+        
         if ($user->platforms()->where('platform_id', $request->platform_id)->exists()) {
             $user->platforms()->detach($request->platform_id);
-            return response()->json(['message' => 'Platform detached']);
+            return $this->responseHelper->success('Platform detached successfully.');
         } else {
             $user->platforms()->attach($request->platform_id);
-            return response()->json(['message' => 'Platform attached']);
+            return $this->responseHelper->success('Platform attached successfully.');
         }
     }
 }
