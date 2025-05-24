f7aeab3b49cf73feb1131f83ec96dbfb4e91554a
omarabuelkhier <omarabuelkhier@gmail.com>
2025-05-24 13:32:12 +0300

    Add PlatformFactory for generating Platform model test data

---
+++ database/factories/PlatformFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Platform>
+ */
+class PlatformFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->unique()->company(),
+            'type' => fake()->randomElement(['twitter', 'instagram', 'linkedin', 'facebook', 'tiktok', 'youtube']),
+        ];
+    }
+}
