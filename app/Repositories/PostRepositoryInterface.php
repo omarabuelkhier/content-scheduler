7acb88d90d6c6b9a7906851cf0d09f14090b8e3e
omarabuelkhier <omarabuelkhier@gmail.com>
2025-05-25 17:18:33 +0300

    Add pagination parameters to getUserPosts and getAllPosts methods in PostRepositoryInterface

--- app/Repositories/PostRepositoryInterface.php
+++ app/Repositories/PostRepositoryInterface.php
@@ -4,8 +4,8 @@ namespace App\Repositories;
 
 interface PostRepositoryInterface extends BaseRepositoryInterface
 {
-    public function getUserPosts(int $userId, array $filters = []);
-    public function getAllPosts(array $filters = []);
+    public function getUserPosts(int $userId, array $filters = [], int $perPage );
+    public function getAllPosts(array $filters = [], int $perPage );
     public function getPostById(int $postId);
     public function createPost(array $data);
     public function updatePost(int $postId, array $data);
